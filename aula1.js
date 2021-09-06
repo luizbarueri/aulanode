e191a5b43484b59ca80999d56bd97398fc8fb6f7
luizbarueri <luizbarueri@gmail.com>
2021-09-06 17:02:08 -0300

    primeira aula nodejs

---
+++ aula1.js
@@ -0,0 +1,17 @@
+var nome_contato, idade, telefone, endereco, numero_da_casa, cidade, cep, cpf, rg;
+nome_contato = "Marisa";
+idade = 33
+telefone = "(11) 9999-6767";
+endereco = "Rua Luís de Camoes,";
+numero_da_casa = "105";
+cidade = "Barueri";
+cep = "06435-190";
+cpf = "123.675.345.99";
+rg = "23.678.666-9"
+
+console.log("O nome do contato é " + nome_contato + ", ela tem " + idade + 
+" anos de idade, o número de contato dela é " + telefone + ",\n" +
+"mora no endereço " + endereco + " com o número " + numero_da_casa + 
+" da residencia, na cidade " + cidade  +
+" com o cep " + cep + " numero de CPF :" + cpf +" e RG: " + rg);
+console.log("FIM");
